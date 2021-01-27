712c5c4f6a2c6b22d68ae40da21b1b1d8b7a7882
Your Name <you@example.com>
2021-01-26 21:25:32 -0600

    models

--- library/apps/book/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Author(models.Model):
-    name = models.CharField(max_length=200, blank=False, null=False)
-    last_name = models.CharField(max_length=220, blank=False, null=False)
-    nationality = models.CharField(max_length=220, blank=False, null=False)
-    description = models.TextField(blank=False, null=False)
