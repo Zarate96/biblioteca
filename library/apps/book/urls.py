863ed9e68776f90120e9ea6bf5224defe6995453
Your Name <you@example.com>
2021-01-27 14:24:11 -0600

    crud

--- library/apps/book/urls.py
+++ library/apps/book/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import createAuthor
+from .views import *
 
 urlpatterns = [
-    path('create_author/', createAuthor, name="createAuthor")
+    path('create_author/', createAuthor, name="createAuthor"),
+    path('list_author/', listAuthor, name="listAuthor"),
+    path('update_author/<int:id>', updateAuthor, name="updateAuthor"),
+    path('delete_author/<int:id>', deleteAuthor, name="deleteAuthor"),
 ]
--- library/apps/book/views.py
+++ library/apps/book/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render,redirect
 from .forms import AuthorForm
-
+from .models import Author
 # Create your views here.
 
 def Home(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', {'title':'Home'})
 
 def createAuthor(request):
     if request.method == 'POST':
@@ -14,4 +14,26 @@ def createAuthor(request):
             return redirect('home')
     else:
         author_form = AuthorForm()
+    return render(request, 'book/createAuthor.html', {'author_form':author_form, 'title':'Create author'})
+
+def listAuthor(request):
+    authors = Author.objects.all()
+    return render(request, 'book/list_author.html', {'authors':authors, 'title':'List author'})
+
+def updateAuthor(request,id):
+    author = Author.objects.get(pk=id)
+    if request.method == 'GET':
+        author_form = AuthorForm(instance=author)
+    else:
+        author_form = AuthorForm(request.POST, instance=author)
+        if author_form.is_valid():
+            author_form.save()
+            return redirect('home')
     return render(request, 'book/createAuthor.html', {'author_form':author_form})
+
+def deleteAuthor(request,id):
+    author = Author.objects.get(pk=id)
+    if request.method == 'POST':
+        author.delete()
+        return redirect('book:listAuthor')
+    return render(request,'book/deleteAuthor.html', {'author':author, 'title':'Delete Author'})
