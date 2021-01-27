91fe9aeab21381321fe4b34a14b28f31236f8028
Your Name <you@example.com>
2021-01-27 13:12:15 -0600

    templates

--- library/apps/book/views.py
+++ library/apps/book/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import AuthorForm
 
 # Create your views here.
+
+def Home(request):
+    return render(request, 'index.html')
+
+def createAuthor(request):
+    if request.method == 'POST':
+        author_form = AuthorForm(request.POST)
+        if author_form.is_valid():
+            author_form.save()
+            return redirect('home')
+    else:
+        author_form = AuthorForm()
+    return render(request, 'book/createAuthor.html', {'author_form':author_form})
--- library/library/urls.py
+++ library/library/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from apps.book.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('book/', include(('apps.book.urls','book')))
+    path('book/', include(('apps.book.urls','book'))),
+    path('home/', Home, name='home'),
 ]
